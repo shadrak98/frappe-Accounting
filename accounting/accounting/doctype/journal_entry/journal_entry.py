1471700ad5b4cf29f50f1d84ac0f969c44f66818
shadrak gurupnor <gurupnor.shadrak@gmail.com>
2021-03-16 20:27:11 +0530

    Journal entry updated

--- accounting/accounting/doctype/journal_entry/journal_entry.py
+++ accounting/accounting/doctype/journal_entry/journal_entry.py
@@ -5,6 +5,32 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+from accounting.accounting.general_ledger import make_gl_entry, make_reverse_gl_entry
 
 class JournalEntry(Document):
-	pass
+	
+	def validate(self):
+		check_total(self)
+		if self.difference:
+			frappe.throw("Total Debit and Credit must be equal. The current difference is {}".format(self.difference))
+		if self.total_credit == 0 or self.total_debit == 0:
+			frappe.throw("Total Debit or Credit can't be Zero.")
+		if not self.accounts:
+			frappe.throw("Accounts Entries are mandatory.")
+
+	
+	def on_submit(self):
+		for entry in self.accounts:
+			make_gl_entry(self, entry.account, entry.debit, entry.credit)
+
+	def on_cancel(self):
+		make_reverse_gl_entry(self, self.doctype, self.name)
+
+def check_total(self):
+	self.total_debit, self.total_credit, self.difference = 0, 0, 0
+	for entry in self.accounts:
+		self.total_debit = flt(self.total_debit) + flt(entry.debit)
+		self.total_credit = flt(self.total_credit) + flt(entry.credit)
+
+	self.difference = flt(self.total_debit) - flt(self.total_credit)
---
+++ accounting/accounting/general_ledger.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import now
+
+def make_gl_entry(self, account, debit, credit):
+    gl_entry = frappe.get_doc({
+        'doctype': 'GL Entry',
+        'posting_date': self.posting_date,
+        'account': account,
+        'voucher_type': self.doctype,
+        'voucher_no': self.name,
+        'debit': debit,
+        'credit': credit,
+        'company': self.company
+    })
+    gl_entry.insert()
+
+
+def make_reverse_gl_entry(self, voucher_type, voucher_no):
+    gl_entries = frappe.get_all("GL Entry",
+                    fields = ['*'],
+                    filters = {
+                        "voucher_type": voucher_type,
+                        "voucher_no": voucher_no,
+                        "is_cancelled": 0
+                    })
+
+    if gl_entries:
+        set_as_cancel(gl_entries[0]['voucher_type'],gl_entries[0]['voucher_no'])
+
+        for entry in gl_entries:
+            entry['name'] = None
+            debit = entry.get('debit', 0)
+            credit = entry.get('credit', 0)
+
+            entry['debit'] = credit
+            entry['credit'] = debit
+
+            entry['is_cancelled'] = 1
+            if entry['debit'] or entry['credit']:
+                make_reverse_entry(entry)
+
+def make_reverse_entry(entry):
+    gle = frappe.new_doc('GL Entry')
+    gle.update(entry)
+    gle.insert()
+
+
+def set_as_cancel(voucher_type, voucher_no):
+    frappe.db.sql("""UPDATE `tabGL Entry` SET is_cancelled = 1,
+                modified=%s, modified_by=%s
+                where voucher_type=%s and voucher_no=%s and is_cancelled = 0""",
+                (now(), frappe.session.user, voucher_type, voucher_no))
