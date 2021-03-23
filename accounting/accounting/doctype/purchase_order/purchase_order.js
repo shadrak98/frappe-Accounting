9ad98bfb47a857d3bbd08e0847d49bbf55e0056c
shadrak gurupnor <gurupnor.shadrak@gmail.com>
2021-03-23 16:49:22 +0530

    added functionalities

--- accounting/accounting/doctype/purchase_order/purchase_order.js
+++ accounting/accounting/doctype/purchase_order/purchase_order.js
@@ -1,8 +1,54 @@
 // Copyright (c) 2021, shadrak and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Purchase Order', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on('Purchase Order', 'onload', function(frm) {
+	frm.set_query("supplier", function (){
+		return {
+			"filters": {
+				"party_type": "Supplier"
+			}
+		}
+	})
 });
+
+frappe.ui.form.on('Purchase Order Item', {
+	item_name(frm, cdt, cdn) {
+		//let row = frappe.get_doc(cdt, cdn);
+		let row = locals[cdt][cdn];
+		let qty = row.item_quantity;
+		let rate = row.item_rate;
+		let total = qty * rate;
+		row.amount = total;
+		// frm.refresh_field('amount');
+		calculate_total(frm);
+		frm.refresh();
+	},
+	item_quantity(frm, cdt, cdn) {
+		//let row = frappe.get_doc(cdt, cdn);
+		let row = locals[cdt][cdn];
+		let qty = row.item_quantity;
+		let rate = row.item_rate;
+		let total = qty * rate;
+		row.amount = total;
+		frm.refresh_field('amount');
+		calculate_total(frm);
+		frm.refresh();
+	}
+});
+
+var calculate_total = function(frm) {
+	var total = 0;
+	var quantity = 0;
+	var items = frm.doc.items;
+	console.log(frm);
+	for (var d in items) {
+		total = total + items[d].amount;
+		quantity = quantity + items[d].item_quantity;
+		// console.log(total + " " + quantity);
+	} 
+	// frm.doc.total_quantity = quantity;
+	// frm.doc.total_amount = flt(total);
+	// frm.refresh_field('total_amount');
+	frm.set_value('total_amount', flt(total));
+	frm.set_value('total_quantity', quantity);
+}
